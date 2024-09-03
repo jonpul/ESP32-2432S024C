f05afed828b92a6f7d2fc51eea479f1766e23489
jonpul <jonpul@msn.com>
2024-09-03 14:13:19 -0500

    Create secrets.h

---
+++ GlanceRectDisplay/secrets.h
@@ -0,0 +1,8 @@
+// Replace with your network details
+const char* SSID = "<SSID>";
+const char* SSID_PASSWORD = "<WIFI PWD>";
+const char* BACKUP_SSID = "<BACKUP SSID>";
+const char* BACKUP_SSID_PASSWORD = "<BACKUP WIFI PWD>";
+const char* THOUGHT_USERID = "<USER ID>";     // https://www.stands4.com/services/v2/quotes.php?uid=<user ID>&tokenid=<token>>&format=json  << you will need a free account on www.stands4.com to call this endpoint
+const char* THOUGHT_TOKEN = "<TOKEN>";   
+
